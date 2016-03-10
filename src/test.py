b23ec88d0656e8234e28bd99c7ef465f8f449e64
John Ko <johnko426@gmail.com>
2016-03-09 22:40:23 -0800

    Updated test py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,10 @@
-
-
+import numpy as np
+import sys
+sys.path.append("../")
+import mltools as ml
+import mltools.linear
+import matplotlib.pyplot as plt
+import mltools.dtree as dtree
 
 class LowestMSE:
 
@@ -17,7 +22,7 @@ class LowestMSE:
             self.nBags = nBags
             self.maxDepth = maxDepth
             self.minParent = minParent
-            print nBags, maxDepth, minParent
+            print mse, nBags, maxDepth, minParent
 
     def __str__(self):
         return "Lowest MSE: " + str(self.mse) + ", nbags:" + str(self.nBags) + ", maxdepth:" + str(self.maxDepth) + ", minparent:" + str(self.minParent)
@@ -36,28 +41,27 @@ def setup_code(xTrainFile, yTrainFile):
     #maxDepth
     ########################
 
-    nBags = 25
+    nBags = 6000
     YtHat = np.zeros((M,nBags))
     YvHat = np.zeros((Mv,nBags))
     rforest = [None] * nBags
 
-    maxDepth = 100
+    maxDepth = 30
     lowestMaxDepth = LowestMSE()
     nFeatures = 60
     minParent = 8
 
-    for l in range(10, nBags):
-        for mDepth in range(10, maxDepth):
+    for l in range(1,nBags):
+        print "bags", l
+        Xi,Yi = ml.bootstrapData(Xtr,Ytr, M)
 
-            Xi,Yi = ml.bootstrapData(Xtr,Ytr, M)
-
-            rforest[l] = dtree.treeRegress()
-            rforest[l].train(Xi,Yi,maxDepth=mDepth,nFeatures=nFeatures, minParent=minParent)
-            YtHat[:,l] = rforest[l].predict(Xtr)[:,0] # predict on training data
-            YvHat[:,l] = rforest[l].predict(Xte)[:,0]
-            mseT = ((Ytr - YtHat[:,0:l].mean(axis=1))**2).mean()
-            mseV = ((Yte - YvHat[:,0:l].mean(axis=1))**2).mean()
-            lowestMaxDepth.set(mseV, l, mDepth, minParent, mDepth)
+        rforest[l] = dtree.treeRegress()
+        rforest[l].train(Xi,Yi,maxDepth=30)
+        YtHat[:,l] = rforest[l].predict(Xtr)[:,0] # predict on training data
+        YvHat[:,l] = rforest[l].predict(Xte)[:,0]
+        mseT = ((Ytr - YtHat[:,0:l].mean(axis=1))**2).mean()
+        mseV = ((Yte - YvHat[:,0:l].mean(axis=1))**2).mean()
+        lowestMaxDepth.set(mseV, l, 30, minParent, l)
     
 
     print "Lowest"
@@ -66,8 +70,7 @@ def setup_code(xTrainFile, yTrainFile):
     
 def write_to_file(x, y):
     fh = open('MSEprogress.txt','a')    # open file for upload
-    fh.write('ID,Prediction\n')         # output header line
     fh.write('{},{}\n'.format(x,y)) # output each prediction
     fh.close()
 
-setup_code("/home/john/Downloads/kaggle.X1.train.txt", "/home/john/Downloads/kaggle.y.train.txt")
+setup_code("../../../../Downloads/kaggle.X1.train.txt", "../../../../Downloads/kaggle.Y.train.txt")
